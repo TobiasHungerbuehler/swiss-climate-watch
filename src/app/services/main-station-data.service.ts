8a9bf48a271e2e6f58a7902ed08b549722ec2f30
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-06-21 22:04:19 +0200

    main station data works

--- src/app/services/main-station-data.service.ts
+++ src/app/services/main-station-data.service.ts
@@ -1,61 +1,32 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 import { TemperatureService, CurrentTemp } from './temperature.service';
-import { ReferenceDataService, ReferenceData } from './reference-data.service';
+import { ReferenceDataService } from './reference-data.service';
 import { DataDisplayService } from './data-display.service';
 import { DayAverageTemperatureService } from './day-average.service';
 import { StandardStationDataService, StandardStationData } from './standard-station-data.service';
 
-
-export interface MainStationData {
-  cityName: string;
-  top: string;
-  left: string;
-  city: string;
-  als: number;
-  currentTemp: number;
-  refTemp: number;
-  anomaly: number;
-}
+export interface MainStationData extends StandardStationData {}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainStationDataService {
-  private mainStationData: MainStationData[] = [
-    { cityName: "Zürich", top: '22%', left: '57%', city: 'SMA', als: 556, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "St.Gallen", top: '19%', left: '75%', city: 'STG', als: 775, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Säntis", top: '26%', left: '74%', city: 'SAE', als: 2501, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Sion", top: '75%', left: '32%', city: 'SIO', als: 482, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Samedan", top: '60%', left: '86%', city: 'SAM', als: 1708, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Basel", top: '14%', left: '37%', city: 'BAS', als: 316, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Bern", top: '42%', left: '34%', city: 'BER', als: 553, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Genf", top: '76%', left: '6%', city: 'GEA', als: 376, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Locarno", top: '79%', left: '63%', city: 'OTL', als: 367, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Luzern", top: '37%', left: '52%', city: 'LUZ', als: 455, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Davos", top: '48%', left: '87%', city: 'DAV', als: 1594, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Engelberg", top: '47%', left: '55%', city: 'ENG', als: 1035, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Meiringen", top: '52%', left: '48%', city: 'MER', als: 588, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Andermatt", top: '56%', left: '58%', city: 'ANT', als: 1438, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Bad Ragaz", top: '38%', left: '77%', city: 'RAG', als: 496, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Elm", top: '43%', left: '71%', city: 'ELM', als: 957, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "La Chaux-de-Fonds", top: '35%', left: '21%', city: 'CDF', als: 1017, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Payerne", top: '47%', left: '23%', city: 'PAY', als: 490, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Château-d'Oex", top: '66%', left: '26%', city: 'CHD', als: 1028, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Lugano", top: '86%', left: '66.5%', city: 'LUG', als: 273, currentTemp: 0, refTemp: 0, anomaly: 0 },
-    { cityName: "Grand St-Bernard", top: '90%', left: '27%', city: 'GSB', als: 2472, currentTemp: 0, refTemp: 0, anomaly: 0 },
-  ];
-
+  private mainStationData: MainStationData[] = this.standardStationDataService.getStandardStationData();
   private mainStationDataSubject = new BehaviorSubject<MainStationData[]>(this.mainStationData);
+
   private highestTempSubject = new BehaviorSubject<number>(0); // BehaviorSubject für höchste Temperatur
 
   constructor(
     private temperatureService: TemperatureService,
     private referenceDataService: ReferenceDataService,
     private dataDisplayService: DataDisplayService,
-    private dayAverageTemperatureService: DayAverageTemperatureService
+    private dayAverageTemperatureService: DayAverageTemperatureService,
+    private standardStationDataService: StandardStationDataService
   ) {
     this.subscribeToDisplayMode();
+    this.loadReferenceTemperatures();
   }
 
   // Observable für die Hauptstationdaten
@@ -73,11 +44,9 @@ export class MainStationDataService {
     this.dataDisplayService.getDisplayMode().subscribe(mode => {
       if (mode === 'current') {
         this.subscribeToTemperatureUpdates();
-        this.loadReferenceTemperatures();
       } 
       if (mode === 'dayAverage') {
         this.subscribeToDayAverage();
-
       } 
     });
   }
@@ -86,15 +55,14 @@ export class MainStationDataService {
   private loadReferenceTemperatures(): void {
     const referenceData = this.referenceDataService.getReferenceData();
     const currentMonth = this.getMonth().toString(); // Konvertiere die Monatszahl in einen String
-    referenceData.forEach(ref => {
-      const station = this.mainStationData.find(station => station.city === ref.city);
-      if (station && ref.referenceTemp[currentMonth as keyof typeof ref.referenceTemp]) {
+    this.mainStationData.forEach(station => {
+      const ref = referenceData.find(r => r.city === station.city);
+      if (ref && ref.referenceTemp[currentMonth as keyof typeof ref.referenceTemp]) {
         station.refTemp = ref.referenceTemp[currentMonth as keyof typeof ref.referenceTemp].average;
         station.anomaly = station.currentTemp - station.refTemp;
       }
     });
     this.mainStationDataSubject.next(this.mainStationData);
-    console.log(this.mainStationData); // Kontrollausgabe der aktualisierten Referenzdaten
   }
 
   // Funktion zum Abrufen des aktuellen Monats als Zahl (1-12)
@@ -105,68 +73,58 @@ export class MainStationDataService {
 
   // Abonniere die Temperaturupdates vom TemperatureService
   private subscribeToTemperatureUpdates(): void {
-    this.temperatureService.getCurrentTempObservable().subscribe(currentTemps => {
+    combineLatest([
+      this.temperatureService.getCurrentTempObservable(),
+      this.dataDisplayService.getDisplayMode()
+    ]).pipe(
+      filter(([, mode]) => mode === 'current')
+    ).subscribe(([currentTemps]) => {
       this.updateCurrentTemps(currentTemps);
       this.calculateAndSetHighestTemp();
     });
   }
 
-
   // Abonniere die durchschnittlichen Tagestemperaturen vom DayAverageTemperatureService
   private subscribeToDayAverage(): void {
-    this.dayAverageTemperatureService.getAverageTemperatures().subscribe(avgTemps => {
+    combineLatest([
+      this.dayAverageTemperatureService.getAverageTemperatures(),
+      this.dataDisplayService.getDisplayMode()
+    ]).pipe(
+      filter(([, mode]) => mode === 'dayAverage')
+    ).subscribe(([avgTemps]) => {
       this.updateDayAverageTemps(avgTemps);
     });
   }
 
-  // Aktualisiere die durchschnittlichen Tagestemperaturen
-  private updateDayAverageTemps(avgTemps: any[]): void {
-    const mainStationData = this.mainStationDataSubject.getValue();
-    avgTemps.forEach(temp => {
-      const station = mainStationData.find(station => station.city === temp.city);
-      if (station) {
-        station.currentTemp = temp.avg_temp;
-        station.anomaly = station.currentTemp - station.refTemp;
-      }
-    });
-    this.mainStationDataSubject.next(mainStationData);
-    this.calculateAndSetHighestTemp();
-    console.log(mainStationData); // Kontrollausgabe der aktualisierten Daten
-  }
-
-
-
-
-
-
-
-
-
-
+// Aktualisiere die durchschnittlichen Tagestemperaturen
+private updateDayAverageTemps(avgTemps: any[]): void {
+  this.mainStationData.forEach(station => {
+    const temp = avgTemps.find(t => t.city === station.city);
+    if (temp && !isNaN(temp.avg_temp)) {
+      const avgTemp = parseFloat(temp.avg_temp);
+      station.currentTemp = parseFloat(avgTemp.toFixed(1));
+      station.anomaly = parseFloat((station.currentTemp - station.refTemp).toFixed(1));
+    }
+  });
+  this.mainStationDataSubject.next(this.mainStationData);
+  this.calculateAndSetHighestTemp();
+}
 
   // Aktualisiere die aktuellen Temperaturen und Anomalien
   private updateCurrentTemps(currentTemps: CurrentTemp[]): void {
-    currentTemps.forEach(temp => {
-      const station = this.mainStationData.find(station => station.city === temp.city);
-      if (station) {
+    this.mainStationData.forEach(station => {
+      const temp = currentTemps.find(t => t.city === station.city);
+      if (temp) {
         station.currentTemp = temp.currentTemp;
         station.anomaly = station.currentTemp - station.refTemp;
       }
     });
     this.mainStationDataSubject.next(this.mainStationData);
-    this.calculateAndSetHighestTemp(); // Verschiebe die Berechnung hierher
-    //console.log(this.mainStationData); // Kontrollausgabe
   }
 
   // Berechne und setze die höchste Temperatur
   private calculateAndSetHighestTemp(): void {
     const highestTemp = this.mainStationData.length === 0 ? 0 : Math.max(...this.mainStationData.map(station => station.currentTemp));
     this.highestTempSubject.next(highestTemp);
-    //console.log('Highest Temperature:', highestTemp); // Kontrollausgabe der höchsten Temperatur
-  }
-
-  // Lade historische Daten (noch nicht implementiert)
-  private loadHistoricalData(): void {
-    // Implementiere das Laden historischer Daten hier
   }
 }
