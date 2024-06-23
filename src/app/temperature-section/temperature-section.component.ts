51b9adafa829fb1bb111a225a88878f92b65beca
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-06-23 15:06:37 +0200

    temperature section

---
+++ src/app/temperature-section/temperature-section.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MapComponent } from '../map/map.component';
+import { LocalMarkerComponent } from '../map/local-marker/local-marker.component';
+import { DataToggleComponent } from '../data-toggle/data-toggle.component';
+
+@Component({
+  selector: 'app-temperature-section',
+  standalone: true,
+  imports: [CommonModule, MapComponent, DataToggleComponent],
+  templateUrl: './temperature-section.component.html',
+  styleUrl: './temperature-section.component.scss'
+})
+export class TemperatureSectionComponent {
+
+}
