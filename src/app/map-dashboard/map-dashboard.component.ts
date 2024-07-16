2455dcabe95d35853749251703486c2aa6828e06
Tobias Hungerbühler <76483987+TobiasHungerbuehler@users.noreply.github.com>
2024-07-16 18:25:48 +0200

    map dashboard component

--- src/app/map-dashboard/map-dashboard.component.ts
+++ src/app/map-dashboard/map-dashboard.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MapDisplayComponent } from '../map-display/map-display.component';
 
 @Component({
   selector: 'app-map-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MapDisplayComponent],
   templateUrl: './map-dashboard.component.html',
   styleUrl: './map-dashboard.component.scss'
 })
