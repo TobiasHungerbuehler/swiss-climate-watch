3aecf2f63996418671635fa647ce344ddb0feb48
Tobias Hungerbühler <to.hungerbuehler@gmail.com>
2024-08-04 11:01:56 +0200

    no

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
+import { MainContentComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
