e568e4df6920660092ce7978164e0dfea6aa1f81
TobiasHungerbuehler <to.hungerbuehler@gmail.com>
2025-01-21 09:38:25 +0100

    onboard texte, mobile scroll

--- src/app/dashboard/onboarding/onboarding.component.ts
+++ src/app/dashboard/onboarding/onboarding.component.ts
@@ -1,35 +1,33 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-onboarding',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './onboarding.component.html',
-  styleUrl: './onboarding.component.scss'
+    selector: "app-onboarding",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./onboarding.component.html",
+    styleUrl: "./onboarding.component.scss",
 })
 export class OnboardingComponent {
+    @Input() displayMode: "current" | "dayAverage" | "monthAverage" = "current";
 
-  @Input() displayMode: 'current' | 'dayAverage' | 'monthAverage' = 'current';
-  
-  constructor(){}
+    constructor() {}
 
-  onboardingText = {
-      "current": {
-        "text": "Diese Karte zeigt die aktuellen Temperaturen an verschiedenen Messstationen in der Schweiz, aktualisiert alle 10 Minuten. Wir sammeln diese Daten kontinuierlich, um sie für weitere Analysen zu verwenden. Durch die Aufzeichnung können wir Durchschnittstemperaturen der letzten 24 Stunden für jede Messstation erstellen.",
-        "buttontext": "zeige letzte 24h"
-      },
-      "dayAverage": {
-        "text": "Hier vergleichen wir die Durchschnittstemperaturen der letzten 24 Stunden mit den historischen Durchschnittswerten von 1940 bis 1970. Dieser Ansatz berücksichtigt die Temperaturschwankungen innerhalb eines Tages und bietet einen genaueren Überblick über aktuelle Abweichungen. Für eine detailliertere Analyse sind jedoch monatliche Durchschnittswerte aussagekräftiger.",
-        "buttontext": "Weiter"
-      },
-      "monthAverage": {
-        "text": "Auf dieser Seite sehen Sie die tatsächlichen Temperaturunterschiede im Vergleich zum Referenzzeitraum 1940 bis 1970. Sie können einen beliebigen Monat auswählen, um die Veränderungen an allen Messstationen zu betrachten. Die Daten zeigen deutlich, wie sich das Klima in den letzten Jahrzehnten entwickelt hat.\nMeteoSchweiz stellt die monatlichen Durchschnittswerte zur Verfügung, wodurch wir genauere Zahlen erhalten und unseren Vergleich zu früher präzise abbilden können.",
-        "buttontext": "Weiter"
-      }
-    }
-
-
-
+    onboardingText = {
+        current: {
+            text: "Diese Karte zeigt die aktuellen Temperaturen an verschiedenen Messstationen in der Schweiz, aktualisiert alle 10 Minuten.",
+            textB: "Wir sammeln diese Daten kontinuierlich, um sie für weitere Analysen zu verwenden. Durch die Aufzeichnung können wir Durchschnittstemperaturen der letzten 24 Stunden für jede Messstation erstellen.",
+            buttontext: "zeige letzte 24h",
+        },
+        dayAverage: {
+            text: "Vergleich Durchschnittstemperaturen der letzten 24 Stunden mit den historischen Durchschnittswerten von 1940 bis 1970.",
+            textB: "Dieser Ansatz berücksichtigt die Temperaturschwankungen innerhalb eines Tages und bietet einen genaueren Überblick über aktuelle Abweichungen. Für eine detailliertere Analyse sind jedoch monatliche Durchschnittswerte aussagekräftiger.",
+            buttontext: "Weiter",
+        },
+        monthAverage: {
+            text: "Die Monatsdurschnitte im Vergleich zum Referenzzeitraum 1940 bis 1970.",
+            textB: "Sie können einen beliebigen Monat auswählen, um die Veränderungen an allen Messstationen zu betrachten. Die Daten zeigen deutlich, wie sich das Klima in den letzten Jahrzehnten entwickelt hat.\nMeteoSchweiz stellt die monatlichen Durchschnittswerte zur Verfügung, wodurch wir genauere Zahlen erhalten und unseren Vergleich zu früher präzise abbilden können.",
+            buttontext: "Weiter",
+        },
+    };
 }
