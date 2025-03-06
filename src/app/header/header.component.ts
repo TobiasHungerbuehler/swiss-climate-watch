0a87badbaf6aa77cffef3cec34308ea6809fa23a
TobiasHungerbuehler <to.hungerbuehler@gmail.com>
2025-03-06 10:59:19 +0100

    router links und upload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: "app-header",
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
